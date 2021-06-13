0f2995bce58d7d56b13d921bda034f2ff373d2e0
snacktime81 <tth8181@gmail.com>
2021-06-13 22:13:55 +0900

    9095_baekjoon_solution

--- 9095_baekjoon.py
+++ 9095_baekjoon.py
@@ -1,15 +1,15 @@
 #9095
-
+# f(n) = f(n-1) + f(n-2) + f(n-3)
 import sys
 
 T = int(sys.stdin.readline())
-cnt = []
 
 for _ in range(T):
   n = int(sys.stdin.readline())
-  k = 1
+  cnt = [0 for _ in range(n)]
   for i in range(n):
-    if k == 1:
-      cnt.append(1)
-    else:
-      
+    if i <= 2:
+      cnt[i] = 2 ** i
+    else: 
+      cnt[i] = cnt[i-1] + cnt[i-2] + cnt[i-3]
+  print(cnt[n-1])
