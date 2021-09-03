1d8ca85f19b71555aa8e82fe6f94c35490d30992
snacktime81 <tth8181@gmail.com>
2021-09-03 15:37:35 +0900

    2805_baekjoon_solution

--- 2805_baekjoon.py
+++ 2805_baekjoon.py
@@ -5,23 +5,18 @@ n, m =map(int, sys.stdin.readline().split())
 trees = list(map(int, sys.stdin.readline().split()))
 k = max(trees)
 
-bottom = 0
+bottom = 1
 top = k
+result = 0
 while bottom <= top:
   mid = (bottom+top) // 2
   wood = 0
   for i in trees:
-    left = i - mid
-    if left > 0:
-      wood += left
-    else:
-      continue
+    if i > mid:
+      wood += i-mid
   if wood < m:
-    bottom = mid + 1
-  elif wood > m:
-    bottom = mid - 1
+    top = mid - 1
   else:
-    print(top)
-    break
-
-  
+    result = mid
+    bottom = mid + 1
+print(result)
