085ea94ce86f63d92a6c3744009a498ebe2e7d59
snacktime81 <tth8181@gmail.com>
2021-09-11 00:02:21 +0900

    11501_baekjoon_solving2_timeover

--- 11501_baekjoon.py
+++ 11501_baekjoon.py
@@ -9,20 +9,18 @@ for i in range(n):
   profit = 0
   cnt = 0 
   for i, price in enumerate(stocks):
-    if i == 0 and price < max_price:
+    if price < max_price:
       profit -= price
       cnt += 1
-    else:
-      stocks_right = stocks[i:]
-      max_price = max(stocks_right)
-      if price < max_price:
-        profit -= price
-        cnt += 1
-      elif price == max_price:
-        profit += max_price * cnt
-        cnt = 0
+    elif price == max_price:
+      profit += max_price * cnt
+      cnt = 0    
+      try:
+        stocks_right = stocks[i+1:]
+        max_price = max(stocks_right)
+      except:
+        break
   profits.append(profit)
 
 for i in profits:
   print(i)
-    
