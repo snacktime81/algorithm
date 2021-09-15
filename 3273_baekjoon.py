960e1a4d0c6a9eabe35a78d596a3b3602f7e9e9b
snacktime81 <tth8181@gmail.com>
2021-09-15 23:09:18 +0900

    3273_baekjoon_solution

--- 3273_baekjoon.py
+++ 3273_baekjoon.py
@@ -2,6 +2,7 @@
 import sys
 n = int(sys.stdin.readline())
 nums = list(map(int, sys.stdin.readline().split()))
+nums.sort()
 k = int(sys.stdin.readline())
 cnt = 0
 
@@ -10,9 +11,18 @@ for i in nums:
     continue
   else:
     key = k - i
-    if key in nums:
-      cnt += 1
-    else:
-      continue
+    bottom = 0
+    top = n-1
+    while bottom <= top:
+      mid = (top+bottom) // 2
+      if nums[mid] > key:
+        top = mid - 1
+      elif nums[mid] < key:
+        bottom = mid + 1
+      elif nums[mid] == key:
+        cnt += 1
+        break
+      else:
+        continue
   
 print(cnt//2)
