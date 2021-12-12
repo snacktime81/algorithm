fca9d155999081890bd8adc0c9209b5ecfcb7a1e
snacktime81 <tth8181@gmail.com>
2021-12-12 16:18:36 +0900

    2579_baekjoon_solving_greedy is not answer

--- 1931_baekjoon.py
+++ 1931_baekjoon.py
@@ -1,23 +1,21 @@
 #1931
 import sys
 N = int(sys.stdin.readline())
-meeting = {}
-li = []
+meeting = []
 clock = []
 
 # 시간초과
 for i in range(N):
-  time = tuple(sys.stdin.readline().split()) # list는 dict의 key가 될 수 없다, 반면에 tuple은 dict의 key가 될 수 있다.
-  using_time = int(time[1]) - int(time[0])
-  meeting[time] = using_time 
-
-meeting_sort = sorted(meeting.items(), key = lambda x: x[1])
-
+  h1, h2 = map(int, sys.stdin.readline().split())
+  using_time = h2 - h1
+  li = [h1, using_time]
+  meeting.append(li)
+meeting.sort(key=lambda x:x[1])
 can = True # 회의시간이 겹치지 않으면 True 겹치면 False
 count = 0
-for i, j in meeting_sort:
-  k = int(i[1])
-  c = int(i[0])
+for i in meeting:
+  k = i[0] + i[1]
+  c = i[0]
   while c != k + 1:
     if c not in clock:
       c += 1
@@ -27,7 +25,7 @@ for i, j in meeting_sort:
   
   if can:
     a = int(i[0])
-    for _ in range(j-1):
+    for _ in range(i[1]-1):
       a += 1
       clock.append(a)
     count += 1
--- 2579_baekjoon.py
+++ 2579_baekjoon.py
@@ -2,18 +2,32 @@
 import sys
 
 stairs = int(sys.stdin.readline())
-score = []
-sum_score = [0]
+score = [0 for _ in  range(stairs+1)]
+sum_score = 0
 
-for i in range(stairs):
-  score.append(int(sys.stdin.readline()))
-  sum_score.append[0]
-N = True
+for i in range(stairs - 1, -1, -1):
+  score[i] = int(sys.stdin.readline())
+score[stairs] = 0
 
-cnt = 0
+B = True
 
-for i in range(stairs):
-  if i == 0:
-    sum_score[0] == score[0]
-  elif i == 1:
-    sum_score[1] == score[1]
+tmp = 1
+sum_score += score[0]
+cnt = 1
+
+while(cnt < stairs):
+  if tmp == 2:
+    cnt += 2
+    sum_score += score[cnt]
+    tmp = 1
+  else:
+    if score[cnt] > score[cnt+1]:
+      sum_score += score[cnt]
+      tmp += 1
+      cnt += 1
+    else:
+      sum_score += score[cnt+1]
+      tmp = 1
+      cnt += 2
+
+print(sum_score)
