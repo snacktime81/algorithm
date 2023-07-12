d7e663d5a22614fa4e643463a5e33721a41d7912
간식time <tth8181@gmail.com>
2023-07-12 08:30:33 +0000

    1541 baekjoon greedy solution

--- 1541_baekjoon.py
+++ 1541_baekjoon.py
@@ -1,9 +1,35 @@
 #1541
 import sys
+input = sys.stdin.readline
 
-sik = sys.stdin.readline().split('-')
-
-print(sik)
 math = ['+', '-']
 
+sik = list(input().rstrip())
 
+arr = []
+
+num =''
+
+for i in range(len(sik)):
+    if(sik[i] in math):
+        arr.append(str(int(num)))
+        arr.append(sik[i])
+        num = ''
+    else:
+        num += sik[i]
+    if(i == len(sik)-1):
+        arr.append(str(int(num)))
+
+#print(arr)
+
+e = "".join(arr).split('-')
+
+
+for i in range(len(e)):
+    e[i] = eval(e[i])
+
+result = e[0]
+
+for i in range(1, len(e)):
+    result -= e[i]
+print(result)
