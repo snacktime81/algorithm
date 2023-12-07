11bd339c066cd6b620dac42a6a8f2092227237f8
간식time <tth8181@gmail.com>
2023-12-07 13:24:18 +0000

    15685 baekjoon solution

--- 15685_baekjoon.py
+++ 15685_baekjoon.py
@@ -3,79 +3,62 @@ from collections import deque
 from copy import deepcopy
 input = sys.stdin.readline
 
+
+''' 
+아이디어:
+그래프를 오른쪽으로 90도 회전시키고 
+원본 그래프의 끝점에
+회전시킨 그래프를 붙힌다.
+'''
+
 n = int(input())
-graph = [[0 for _ in range(100)] for _ in range(100)]
+graph = [[0 for _ in range(101)] for _ in range(101)]
 move = [(0,1), (-1,0), (0,-1), (1,0)]
 
 
-def turn(graph, x, y): # 그래프, 끝점
-    tmp = [[0 for _ in range(100)] for _ in range(100)]
+def turn(graph, x, y): # 그래프, 끝점 그래프를 오른쪽으로 90도 회전
+    tmp = [[0 for _ in range(101)] for _ in range(101)]
     
-    for i in range(100):
-        for j in range(100):
+    for i in range(101):
+        for j in range(101):
             if(graph[i][j] == 1):
-                tmp[j][99-i] = graph[i][j]
+                tmp[j][100-i] = graph[i][j]
     
-    return [tmp, y, 99-x]
+    return [tmp, y, 100-x]
 
 
-def findEdge(x, y, graph):
-    q = deque([(x, y)])
-    graph[x][y] = 0
-    
-    while q:
-        x, y = q.popleft()
-        for i in move:
-            dx = x + i[0]
-            dy = y + i[1]
-            
-            if(dx < 0 or dy < 0 or dx >= 100 or dy >= 100):
-                continue
-            if(graph[dx][dy] == 0):
-                continue
-            
-            q.append([dx, dy])
-            graph[dx][dy] = 0
-    return x, y
-            
-                
-    
-
-for _ in range(n):
+for _ in range(n): 
     y, x, d, g = map(int, input().split()) # 시작좌표, 방향, 세대
     
-    tmpGraph = [[0 for _ in range(100)] for _ in range(100)]
+    tmpGraph = [[0 for _ in range(101)] for _ in range(101)]
     
-    ex, ey = x + move[d][0], y + move[d][1]
+    ex, ey = x + move[d][0], y + move[d][1] # 1세대
     tmpGraph[x][y] = 1
     tmpGraph[ex][ey] = 1
     
     for _ in range(g):
-        tmp, nx, ny = turn(tmpGraph, ex, ey)
-        dx = ex - nx
+        tmp, nx, ny = turn(tmpGraph, ex, ey) # nx, ny는 오른쪽으로 돌린 그래프의 시작점
+        dx = ex - nx # 회전한 그래프의 시작점을 원본 그래프의 끝점에 붙혀야 함으로, nx + dx = ex 즉 그래프를 회전 시켰기 때문에 좌표값이 일정하게 차이가 나고 그 차이값이 dx,dy 이다.
         dy = ey - ny
         #print('dx, dy: ', dx, dy)
-        for i in range(100):
-            for j in range(100):
+        for i in range(101):
+            for j in range(101):
                 if(tmp[i][j] == 1):
                     #print(i,j)
                     tmpGraph[i + dx][j + dy] = 1
-        ex, ey = findEdge(x, y, deepcopy(tmpGraph))
-    for i in range(100):
-        for j in range(100):
+        ex, ey = y+dx, 100-x+dy # 결국 마지막 지점은 시작 좌표가 회전한 값이다. (중요)
+        print('ex ', ex,ey)
+    for i in range(101):
+        for j in range(101):
             if(tmpGraph[i][j] == 1):
+                print('i,j: ', i, j)
                 graph[i][j] = 1
+    #print()
     
-    
-    # for i in range(100):
-    #     for j in range(100):
-    #         print(graph[i][j], end='')
-    #     print()
-
     
 cnt = 0        
-for i in range(99):
-    for j in range(99):
+for i in range(100):
+    for j in range(100):
         ax, ay = i + 1, j + 1
         bx, by = i, j + 1
         cx, cy = i + 1, j
