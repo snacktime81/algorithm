4d97a285cf6bfad2eaef220554005868be38c67d
간식time <tth8181@gmail.com>
2023-12-08 12:59:30 +0000

    1502 baekjoon dfs dp solution

--- 1520_baekjoon.py
+++ 1520_baekjoon.py
@@ -1,43 +1,49 @@
+import sys
 from collections import deque
+input = sys.stdin.readline
 
-m, n = map(int, input().split())
+n, m = map(int, input().split())
 graph = []
 
-for _ in range(m):
+for _ in range(n):
     graph.append(list(map(int, input().split())))
     
-start = (0, 0)
-
-ex = m-1
-ey = n-1
+x, y = (0, 0)
+ex = n-1
+ey = m-1
 
 move = [(0,1),(0,-1),(1,0),(-1,0)]
 
-cnt = 0
 
-x, y = start
 
-q = deque([(x, y)])
 
-while q:
-    x, y = q.popleft()
+visited = [[-1 for _ in range(m)] for _ in range(n)]
+
+def dfs(x, y):
+    if(x == ex and y == ey):
+        return 1
+    if visited[x][y] != -1:
+        return visited[x][y]
     
-    if( x == ex and y == ey):
-        cnt += 1
-        continue
-    
-    num = graph[x][y]
+    cnt = 0
     
     for i in move:
-        
         dx = x + i[0]
         dy = y + i[1]
         
-        if( dx < 0 or dy < 0 or dx >= m or dy >= n):
+        if(dx < 0 or dy < 0 or dx >= n or dy >= m):
             continue
-        
-        if( graph[dx][dy] < num):
-            q.append([dx,dy])
+        if(graph[x][y] <= graph[dx][dy]):
+            continue
             
+        cnt += dfs(dx, dy)
+        #print(visited)
+    visited[x][y] = cnt 
+    return visited[x][y]
+
+cnt = dfs(x, y)   
+
+for i in visited:
+    print(i)
 print(cnt)
         
