74653a20eccfde3da3d07500b18fb0f5a596c5f4
간식time <tth8181@gmail.com>
2023-12-15 10:50:17 +0000

    2108 baekjoon solution

--- 2108_baekjoon.py
+++ 2108_baekjoon.py
@@ -1,32 +1,61 @@
 #2108
 import sys
+import heapq
+input = sys.stdin.readline
 
-n = int(sys.stdin.readline())
-li = []
-for i in range(n):
-  li.append(int(sys.stdin.readline()))
+n = int(input())
 
-li.sort()
-li_third = []
-for i in li:
-  cnt = li.count(i)
-  li_third.append([cnt, i])
+min_n = 4001
+max_n = -4001
 
-li_third_sort = sorted(li_third, reverse=True)
-if len(li) >= 2:
-  if li_third_sort[0][0] == li_third_sort[1][0]:
-    third = li_third_sort[1][1]
-  else:
-    third = li_third_sort[0][1]
-else:
-  third = li_third_sort[0][1]
+s = 0
+c_minus = [0] * (4001)
+c_plus = [0] * 4001
+
+q = []
+
+for _ in range(n):
+    num = int(input())
+    if(num < min_n):
+        min_n = num
+    if(num > max_n):
+        max_n = num
+    s += num
+    if(num < 0):
+        c_minus[abs(num)] += 1
+    else:
+        c_plus[num] += 1
+    heapq.heappush(q, num)
 
 
-first = round(sum(li)/len(li))
-second = li[round(len(li)/2)]
-four = max(li) - min(li)
+a = 0
 
-print(first)
-print(second)
-print(third)
-print(four)
+m_cnt = max(max(c_minus), max(c_plus))
+
+cnt = 0
+for i in range(4000, 0, -1):
+    if(c_minus[i] == m_cnt):
+        if(cnt == 1):
+            a = -i
+            cnt += 1
+            break
+        else:
+            a = -i
+            cnt += 1
+if(cnt < 2):
+    for i in range(4001):
+        if(c_plus[i] == m_cnt):
+            if(cnt == 1):
+                a = i
+                break
+            else:
+                a = i
+                cnt += 1
+
+for _ in range(n//2+1):
+    r = heapq.heappop(q)
+                
+print(round(s/n))
+print(r)
+print(a)
+print(max_n - min_n)
