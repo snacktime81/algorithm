fc4cae6e9f9a06016e0db2fcd0bc2894ae335190
간식time <tth8181@gmail.com>
2024-01-08 10:39:30 +0000

    1931 baekjoon dp solution

--- 1931_baekjoon.py
+++ 1931_baekjoon.py
@@ -1,36 +1,31 @@
-#1931
+ # 1931
+
 import sys
-N = int(sys.stdin.readline())
-meeting = []
-clock = []
 
-# 시간초과
-for i in range(N):
-  h1, h2 = map(int, sys.stdin.readline().split())
-  using_time = h2 - h1
-  li = [h1, using_time]
-  meeting.append(li)
-meeting.sort(key=lambda x:x[1])
-can = True # 회의시간이 겹치지 않으면 True 겹치면 False
-count = 0
-for i in meeting:
-  k = i[0] + i[1]
-  c = i[0]
-  while c != k + 1:
-    if c not in clock:
-      c += 1
+input = sys.stdin.readline
+
+n = int(input())
+arr = []
+
+check = dict()
+
+pre = 0
+
+for _ in range(n):
+    a, b = map(int, input().split())
+    arr.append([a, b])
+    check[a] = 0
+    check[b] = 0
+arr.sort(key=lambda x: (x[1], x[0]))
+endpoint = 0
+for i in arr:
+    start, end = i
+    
+    if(start >= endpoint):
+        check[end] = pre+1
+        endpoint = end
     else:
-      can = False
-      break
-  
-  if can:
-    a = int(i[0])
-    for _ in range(i[1]-1):
-      a += 1
-      clock.append(a)
-    count += 1
-  else:
-    can = True
-    continue
-
-print(count)
+        check[end] = pre
+    pre = check[end]
+    
+print(check[end])
