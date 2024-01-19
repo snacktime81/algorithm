52b6d06a8fce73e75e0cacc464c581e58a4ab6c9
간식time <tth8181@gmail.com>
2024-01-19 13:53:48 +0000

    14226 baekjoon bfs dp solutoin

--- 14226_baekjoon.py
+++ 14226_baekjoon.py
@@ -9,10 +9,10 @@ heappush(q, (0, 1, 0))
 
 dp[1][0] = 0
 while q:
-    try:
         time, num, clip = heappop(q)
-            
-        if(dp[num + clip][clip] > time + 1):
+        
+        
+        if(num+clip < 2001 and dp[num + clip][clip] > time + 1):
             heappush(q, (time+1, num + clip, clip))
             dp[num+clip][clip] = time+1
 
@@ -22,8 +22,7 @@ while q:
         if(num-1 > 0 and dp[num-1][clip] > time+1):
             heappush(q, (time+1, num-1, clip))
             dp[num-1][clip] = time+1
-    except:
-        break
+
 
 print(min(dp[N]))
 
